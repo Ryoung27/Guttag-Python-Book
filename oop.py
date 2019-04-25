04024d2e88c0ac87fefa58ab67c74196ec20a798
Ryoung27 <RRYoung89@gmail.com>
2019-04-25 12:10:42 -0500

    inheritance

--- oop.py
+++ oop.py
@@ -21,6 +21,49 @@ print(x.real_name)
 print(x.eye_color)
 print(x.planet)
 
+
+class Circle():
+    pi = 3.14
+
+    def __init__(self, radius=1):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * Circle.pi
+
+    def perimeter(self):
+        return 2 * self.radius * Circle.pi
+
+    def report_something(self,name):
+        return 'Report {}'.format(name)
+
+mycircle = Circle()
+
+mycircle.radius
+print(mycircle.area())
+
+class Person():
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def report(self):
+        print("I am {} {}".format(self.first_name, self.last_name))
+
+
+# p.hello()
+
+class Agent(Person):
+
+    def __init__(self, first_name, last_name, code_name):
+
+        Person.__init__(self, first_name, last_name)
+        self.code_name = code_name
+
+x = Agent(first_name='Alan', last_name='Turing', code_name='Hero')
+
+# x.hello()
+
 # class NameOfClass():
 
 #     def __init__(self, param1, param2):
